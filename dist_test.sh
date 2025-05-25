822efcfa9eb56b4757f9c55675927412aa084c5f
tegar-ganang <tegarsatrio10@gmail.com>
2025-05-25 11:13:03 +0700

    dist test

--- dist_test.sh
+++ dist_test.sh
@@ -1,3 +1,24 @@
+# #!/usr/bin/env bash
+
+# CONFIG=$1
+# CHECKPOINT=$2
+# GPUS=$3
+# NNODES=${NNODES:-1}
+# NODE_RANK=${NODE_RANK:-0}
+# PORT=${PORT:-29500}
+# MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
+
+# torchrun --nnodes=$NNODES \
+#     --node_rank=$NODE_RANK \
+#     --master_addr=$MASTER_ADDR \
+#     --nproc_per_node=$GPUS \
+#     --master_port=$PORT \
+#     $(dirname "$0")/test.py \
+#     $CONFIG \
+#     $CHECKPOINT \
+#     --launcher pytorch \
+#     ${@:4}
+
 #!/usr/bin/env bash
 
 CONFIG=$1
@@ -8,7 +29,7 @@ NODE_RANK=${NODE_RANK:-0}
 PORT=${PORT:-29500}
 MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 
-torchrun --nnodes=$NNODES \
+/opt/conda/envs/whisper/bin/torchrun --nnodes=$NNODES \
     --node_rank=$NODE_RANK \
     --master_addr=$MASTER_ADDR \
     --nproc_per_node=$GPUS \
